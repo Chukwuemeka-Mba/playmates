c75d87997c5a40c384dcb6569d4dfc09cfc38e6e
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-03-26 10:43:01 -0400

    :card_file_box: added admin site filters

--- playmates_app/admin.py
+++ playmates_app/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 from .models import Playmate, Note
 # Register your models here.
 
-admin.site.register(Playmate)
+@admin.register(Playmate)
+class PlaymateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'author', 'created_at', 'play_count')
+    list_filter = ('created_at', 'rating', 'age', 'play_count')
+    search_fields = ('name', 'description')
+    ordering = ('created_at', 'play_count')
+
 admin.site.register(Note)
+
